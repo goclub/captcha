a5096c01ad4e38b7f718560acd55238b5f77df5e
nimoc <nimocc@126.com>
2022-04-04 13:46:21 +0800

    options.FontDPI = 90

--- captcha.go
+++ captcha.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Option struct {
-	Width int
-	Height int
+	Width      int
+	Height     int
 	TextLength int
 }
+
 func New(opt Option) (code string, buffer *bytes.Buffer, err error) {
 	if opt.Width == 0 {
 		opt.Width = 150
@@ -20,17 +21,21 @@ func New(opt Option) (code string, buffer *bytes.Buffer, err error) {
 		opt.TextLength = 4
 	}
 	data, err := captcha.New(opt.Width, opt.Height, func(options *captcha.Options) {
-		options.BackgroundColor = color.RGBA{1,1,1,1}
+		options.BackgroundColor = color.RGBA{1, 1, 1, 1}
 		options.CharPreset = "abcdefghijkmnpqrstuvwxyz23456789"
 		options.TextLength = opt.TextLength
+		options.FontScale = 1
+		options.FontDPI = 90
 		return
-	}) ; if err != nil {
-	    return
+	})
+	if err != nil {
+		return
 	}
 	code = data.Text
 	buffer = bytes.NewBuffer([]byte(nil))
-	err = data.WriteImage(buffer) ; if err != nil {
-	    return
+	err = data.WriteImage(buffer)
+	if err != nil {
+		return
 	}
 	return
 }
